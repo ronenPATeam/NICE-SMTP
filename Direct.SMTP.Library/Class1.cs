2ad38cf9c4757a350641c51dd9df8f27095e8cbb
heinrich <heinrich@pateam.co>
2022-10-13 11:19:35 +0100

    Isolated the Email Message class into it's own class. Sealed the SMTP Class.

--- Direct.SMTP.Library/Class1.cs
+++ Direct.SMTP.Library/Class1.cs
@@ -6,13 +6,14 @@ using System.Net.Mail;
 
 namespace Direct.SMTP.Library
 {
+    [DirectSealed]
     [DirectDom("SMTP")]
     [ParameterType(false)]
     public class SMTPLibrary
     {
 
         private static readonly ILog logArchitect = LogManager.GetLogger(Loggers.LibraryObjects);
-        
+
         //Sending Email messages using SMTP server 
         [DirectDom("Send an email using SMTP")]
         [DirectDomMethod("Send {Message}")]
@@ -110,165 +111,167 @@ namespace Direct.SMTP.Library
 
         }
 
-        [DirectDom("Email Message", "SMTP")]
-        public class emailMessage : DirectComponentBase
+
+
+    }
+
+    [DirectDom("Email Message")]
+    public class EmailMessage : DirectComponentBase
+    {
+
+        protected PropertyHolder<string> _Subject = new PropertyHolder<string>("Subject");
+        protected PropertyHolder<string> _ServerDetails = new PropertyHolder<string>("ServerDetails");
+        protected CollectionPropertyHolder<string> _Recipients = new CollectionPropertyHolder<string>("Recipients");
+        protected CollectionPropertyHolder<string> _RecipientsCC = new CollectionPropertyHolder<string>("RecipientsCC");
+        protected CollectionPropertyHolder<string> _Attachements = new CollectionPropertyHolder<string>("Attachements");
+        protected PropertyHolder<string> _Body = new PropertyHolder<string>("Body");
+        protected PropertyHolder<string> _UserName = new PropertyHolder<string>("UserName");
+        protected PropertyHolder<string> _Password = new PropertyHolder<string>("Password");
+        protected PropertyHolder<string> _Sender = new PropertyHolder<string>("Sender");
+        protected PropertyHolder<int> _Port = new PropertyHolder<int>("Port");
+        protected PropertyHolder<bool> _ESSL = new PropertyHolder<bool>("ESSL");
+        protected PropertyHolder<bool> _isHtml = new PropertyHolder<bool>("isHtml");
+        protected PropertyHolder<bool> _useDefaultCredentials = new PropertyHolder<bool>("UseDefaultCredentials");
+        protected PropertyHolder<int> _sendTimeOut = new PropertyHolder<int>("sendTimeOut");
+
+        //List property holder
+        [DirectDomAttribute("Recipients")]
+        [DesignTimeInfo("Recipients")]
+        public DirectCollection<string> Recipients
         {
-
-            protected PropertyHolder<string> _Subject = new PropertyHolder<string>("Subject");
-            protected PropertyHolder<string> _ServerDetails = new PropertyHolder<string>("ServerDetails");
-            protected CollectionPropertyHolder<string> _Recipients = new CollectionPropertyHolder<string>("Recipients");
-            protected CollectionPropertyHolder<string> _RecipientsCC = new CollectionPropertyHolder<string>("RecipientsCC");
-            protected CollectionPropertyHolder<string> _Attachements = new CollectionPropertyHolder<string>("Attachements");
-            protected PropertyHolder<string> _Body = new PropertyHolder<string>("Body");
-            protected PropertyHolder<string> _UserName = new PropertyHolder<string>("UserName");
-            protected PropertyHolder<string> _Password = new PropertyHolder<string>("Password");
-            protected PropertyHolder<string> _Sender = new PropertyHolder<string>("Sender");
-            protected PropertyHolder<int> _Port = new PropertyHolder<int>("Port");
-            protected PropertyHolder<bool> _ESSL = new PropertyHolder<bool>("ESSL");
-            protected PropertyHolder<bool> _isHtml = new PropertyHolder<bool>("isHtml");
-            protected PropertyHolder<bool> _useDefaultCredentials = new PropertyHolder<bool>("UseDefaultCredentials");
-            protected PropertyHolder<int> _sendTimeOut = new PropertyHolder<int>("sendTimeOut");
-
-            //List property holder
-            [DirectDomAttribute("Recipients")]
-            [DesignTimeInfo("Recipients")]
-            public DirectCollection<string> Recipients
+            get
             {
-                get
-                {
 
-                    return this._Recipients.TypedValue;
+                return this._Recipients.TypedValue;
 
-                }
-                set
-                {
-                    this._Recipients.TypedValue = value;
-                }
             }
-
-            [DirectDomAttribute("RecipientsCC")]
-            [DesignTimeInfo("RecipientsCC")]
-            public DirectCollection<string> RecipientsCC
+            set
             {
-                get
-                {
-
-                    return this._RecipientsCC.TypedValue;
-
-                }
-                set
-                {
-                    this._RecipientsCC.TypedValue = value;
-                }
-            }
-
-            [DirectDomAttribute("Attachements")]
-            [DesignTimeInfo("Attachements")]
-            public DirectCollection<string> Attachements
-            {
-                get
-                {
-
-                    return this._Attachements.TypedValue;
-
-                }
-                set
-                {
-                    this._Attachements.TypedValue = value;
-                }
-            }
-
-            //Simple properties
-            [DirectDom("Subject")]
-            public string Subject
-            {
-                get { return _Subject.TypedValue; }
-                set { this._Subject.TypedValue = value; }
-            }
-
-
-            [DirectDom("ServerDetails")]
-            public string ServerDetails
-            {
-                get { return _ServerDetails.TypedValue; }
-                set { this._ServerDetails.TypedValue = value; }
-            }
-            [DirectDom("Body")]
-            public string Body
-            {
-                get { return _Body.TypedValue; }
-                set { this._Body.TypedValue = value; }
-            }
-
-            [DirectDom("UserName")]
-            public string UserName
-            {
-                get { return _UserName.TypedValue; }
-                set { this._UserName.TypedValue = value; }
-            }
-            [DirectDom("Password")]
-            public string Password
-            {
-                get { return _Password.TypedValue; }
-                set { this._Password.TypedValue = value; }
-            }
-
-            [DirectDom("Sender")]
-            public string Sender
-            {
-                get { return _Sender.TypedValue; }
-                set { this._Sender.TypedValue = value; }
-            }
-            [DirectDom("Port")]
-            public int Port
-            {
-                get { return _Port.TypedValue; }
-                set { this._Port.TypedValue = value; }
-            }
-            [DirectDom("ESSL")]
-            public bool ESSL
-            {
-                get { return _ESSL.TypedValue; }
-                set { this._ESSL.TypedValue = value; }
-            }
-            [DirectDom("isHtml")]
-            public bool isHtml
-            {
-                get { return _isHtml.TypedValue; }
-                set { this._isHtml.TypedValue = value; }
-            }
-            [DirectDom("Use Default Credentials")]
-            public bool UseDefaultCredentials
-            {
-                get { return _useDefaultCredentials.TypedValue; }
-                set { this._useDefaultCredentials.TypedValue = value; }
-            }
-            [DirectDom("sendTimeOut")]
-            public int sendTimeOut
-            {
-                get { return _sendTimeOut.TypedValue; }
-                set { this._sendTimeOut.TypedValue = value; }
-            }
-
-
-            //constructors
-            private void initLists()
-            {
-                //init lists
-                this.Recipients = new DirectCollection<string>(this.Project);
-                this.RecipientsCC = new DirectCollection<string>(this.Project);
-                this.Attachements = new DirectCollection<string>(this.Project);
-            }
-            public emailMessage()
-            {
-                initLists();
-            }
-            public emailMessage(Direct.Shared.IProject project)
-                : base(project)
-            {
-                initLists();
+                this._Recipients.TypedValue = value;
             }
         }
 
+        [DirectDomAttribute("RecipientsCC")]
+        [DesignTimeInfo("RecipientsCC")]
+        public DirectCollection<string> RecipientsCC
+        {
+            get
+            {
+
+                return this._RecipientsCC.TypedValue;
+
+            }
+            set
+            {
+                this._RecipientsCC.TypedValue = value;
+            }
+        }
+
+        [DirectDomAttribute("Attachements")]
+        [DesignTimeInfo("Attachements")]
+        public DirectCollection<string> Attachements
+        {
+            get
+            {
+
+                return this._Attachements.TypedValue;
+
+            }
+            set
+            {
+                this._Attachements.TypedValue = value;
+            }
+        }
+
+        //Simple properties
+        [DirectDom("Subject")]
+        public string Subject
+        {
+            get { return _Subject.TypedValue; }
+            set { this._Subject.TypedValue = value; }
+        }
+
+
+        [DirectDom("ServerDetails")]
+        public string ServerDetails
+        {
+            get { return _ServerDetails.TypedValue; }
+            set { this._ServerDetails.TypedValue = value; }
+        }
+        [DirectDom("Body")]
+        public string Body
+        {
+            get { return _Body.TypedValue; }
+            set { this._Body.TypedValue = value; }
+        }
+
+        [DirectDom("UserName")]
+        public string UserName
+        {
+            get { return _UserName.TypedValue; }
+            set { this._UserName.TypedValue = value; }
+        }
+        [DirectDom("Password")]
+        public string Password
+        {
+            get { return _Password.TypedValue; }
+            set { this._Password.TypedValue = value; }
+        }
+
+        [DirectDom("Sender")]
+        public string Sender
+        {
+            get { return _Sender.TypedValue; }
+            set { this._Sender.TypedValue = value; }
+        }
+        [DirectDom("Port")]
+        public int Port
+        {
+            get { return _Port.TypedValue; }
+            set { this._Port.TypedValue = value; }
+        }
+        [DirectDom("ESSL")]
+        public bool ESSL
+        {
+            get { return _ESSL.TypedValue; }
+            set { this._ESSL.TypedValue = value; }
+        }
+        [DirectDom("isHtml")]
+        public bool isHtml
+        {
+            get { return _isHtml.TypedValue; }
+            set { this._isHtml.TypedValue = value; }
+        }
+        [DirectDom("Use Default Credentials")]
+        public bool UseDefaultCredentials
+        {
+            get { return _useDefaultCredentials.TypedValue; }
+            set { this._useDefaultCredentials.TypedValue = value; }
+        }
+        [DirectDom("sendTimeOut")]
+        public int sendTimeOut
+        {
+            get { return _sendTimeOut.TypedValue; }
+            set { this._sendTimeOut.TypedValue = value; }
+        }
+
+
+        //constructors
+        private void initLists()
+        {
+            //init lists
+            this.Recipients = new DirectCollection<string>(this.Project);
+            this.RecipientsCC = new DirectCollection<string>(this.Project);
+            this.Attachements = new DirectCollection<string>(this.Project);
+        }
+        public emailMessage()
+        {
+            initLists();
+        }
+        public emailMessage(Direct.Shared.IProject project)
+            : base(project)
+        {
+            initLists();
+        }
     }
 }
